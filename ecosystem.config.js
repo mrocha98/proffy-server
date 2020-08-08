67937898c5cb47cfb661edb73f10e2aaed1512a5
Matheus Rocha <matheus_rocha98@outlook.com>
2020-08-08 02:02:04 -0300

    kill me pls

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'server',
-      script: 'dist/src/server.js',
+      script: 'src/server.js',
       instances: 'max',
       exec_mode: 'cluster'
     }
